b404820ad0ae4a38043fed531e179ceeac6cda11
James Pringle <jameskpringle@gmail.com>
2016-05-23 15:33:53 -0400

    Bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='qlang',
-    version='0.1.0',
+    version='0.1.1',
     author='James K. Pringle',
     author_email='jpringle@jhu.edu',
     url='http://www.pma2020.org',

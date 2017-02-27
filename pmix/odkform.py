a814e988345fa5749948a13dfe17e0431106244a
James Pringle <jameskpringle@gmail.com>
2017-02-27 16:23:04 -0500

    Add special formatting for group and repeat.

--- pmix/odkform.py
+++ pmix/odkform.py
@@ -2,7 +2,9 @@ import os.path
 
 from pmix.error import OdkformError
 from pmix.odkchoices import Odkchoices
+from pmix.odkgroup import Odkgroup
 from pmix.odkprompt import Odkprompt
+from pmix.odkrepeat import Odkrepeat
 from pmix.workbook import Workbook
 
 
@@ -43,10 +45,21 @@ class Odkform:
     def convert_survey(self, wb):
         """Convert rows and strings of a workbook into better python objects
 
+        Main types are
+
+        - prompt
+        - begin group
+        - end group
+        - begin repeat
+        - end repeat
+        - table
+        - context group (group without field-list appearance)
+
         :param wb: Workbook object representing an XLSForm.
         :return: A list of better python objects
         """
         result = []
+        stack = []
         try:
             survey = wb['survey']
             header = survey[0]
@@ -62,8 +75,55 @@ class Odkform:
                     else:
                         choices = None
                     this_prompt = Odkprompt(json_row, choices)
-                    result.append(this_prompt)
-                # TODO Check for begin group/repeat, end group/repeat, table
+                    if stack:
+                        stack[-1].add(this_prompt)
+                    else:
+                        result.append(this_prompt)
+                elif token['token_type'] == 'begin group':
+                    if not stack or isinstance(stack[-1], Odkrepeat):
+                        group = Odkgroup(json_row)
+                        stack.append(group)
+                    else:
+                        m = 'Unable to add group at row {}'.format(i+1)
+                        raise OdkformError(m)
+                elif token['token_type'] == 'end group':
+                    if stack and isinstance(stack[-1], Odkgroup):
+                        group = stack.pop()
+                        group.add_pending()
+                        if stack:
+                            stack[-1].add(group)
+                        else:
+                            result.append(group)
+                    else:
+                        m = 'Mismatched "end group" at row {}'.format(i+1)
+                        raise OdkformError(m)
+                elif token['token_type'] == 'begin repeat':
+                    if not stack:
+                        repeat = Odkrepeat(json_row)
+                        stack.append(repeat)
+                    else:
+                        m = 'Unable to add repeat at row {}'.format(i+1)
+                        raise OdkformError(m)
+                elif token['token_type'] == 'end repeat':
+                    if stack and isinstance(stack[-1], Odkrepeat):
+                        repeat = stack.pop()
+                        # Stack guaranteed empty at this point
+                        result.append(repeat)
+                    else:
+                        m = 'Mismatched "end repeat" at row {}'.format(i+1)
+                        raise OdkformError(m)
+                elif token['token_type'] == 'table':
+                    if stack and isinstance(stack[-1], Odkgroup):
+                        stack[-1].add_table(json_row)
+                    else:
+                        m = 'Table found outside of field-list group at row {}'
+                        m = m.format(i+1)
+                        raise OdkformError(m)
+                elif token['token_type'] == 'context group':
+                    if stack and isinstance(stack[-1], Odkgroup):
+                        m = ('PPP does not allow a group nested in a '
+                             'field-list group. See row {}').format(i+1)
+                        raise OdkformError(m)
         except KeyError:
             # no survey found
             pass
@@ -77,10 +137,11 @@ class Odkform:
         """
         row_type = row['type']
         if row_type in Odkprompt.response_types + Odkprompt.non_response_types:
-            return {
+            d = {
                 'token_type': 'prompt',
                 'simple_type': row_type
             }
+            return d
         elif row_type.startswith('select_one_external '):
             choice_list = row_type.split(maxsplit=1)[1]
             choices = self.external_choices[choice_list]
@@ -100,21 +161,41 @@ class Odkform:
         elif row_type.startswith('select_one '):
             choice_list = row_type.split(maxsplit=1)[1]
             choices = self.choices[choice_list]
-            return {
+            d = {
                 'token_type': 'prompt',
                 'simple_type': 'select_one',
                 'choice_list': choices
             }
+
+            table_label = 'label' in row.get('appearance', '')
+            table_list = 'list-nolabel' in row.get('appearance', '')
+            if table_label or table_list:
+                d['token_type'] = 'table'
+            return d
         elif row_type.startswith('select_multiple '):
             choice_list = row_type.split(maxsplit=1)[1]
             choices = self.choices[choice_list]
-            return {
+            d = {
                 'token_type': 'prompt',
                 'simple_type': 'select_multiple',
                 'choice_list': choices
             }
-        # TODO add in support for "begin group", "end group", "begin repeat",
-        # TODO "end repeat", and "table" here
+
+            table_label = 'label' in row.get('appearance', '')
+            table_list = 'list-nolabel' in row.get('appearance', '')
+            if table_label or table_list:
+                d['token_type'] = 'table'
+            return d
+        elif row_type == 'begin group':
+            d = {'token_type': 'context group'}
+            appearance = row.get('appearance', '')
+            if 'field-list' in appearance:
+                d['token_type'] = row_type
+            return d
+        elif row_type in ('end group', 'begin repeat', 'end repeat'):
+            return {
+                'token_type': row_type
+            }
         else:
             return {'token_type': None}
 

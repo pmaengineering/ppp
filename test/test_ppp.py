031e8dae926e1e63814cf3c00ec8f76e3fc8a20c
joeflack4 <joeflack4@gmail.com>
2017-05-31 14:29:16 -0400

    Refactored test_ppp.py

--- test/test_ppp.py
+++ test/test_ppp.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Unit tests for verbiage.py"""
-
-# Unittest bad-ness:
-# Consider package /pmix/pmix/test
-# from /pmiOdkPrompt, write python -m pmix.test.test_verbiage
-# from /pmiOdkPrompt, write python -m pmix.test (when I get a __main__.py)
-# from /pmiOdkPrompt, write python -m unittest # This is a shortcut for the next line
-# from /pmiOdkPrompt, write python -m unittest discover
-
-# import os
-# import sys
-# print("PATH")
-# print(*sys.path, sep='\n')
-# print("CWD", os.getcwd())
-# print("name", __name__)
-# print("pacakge", __package__)
-
+"""Unit tests for ppp.py"""
 import unittest
 from os import path as os_path
 from pmix.odkform import OdkForm
 from pmix.odkprompt import OdkPrompt
-# from pmix.odkchoices import Odkchoices
+# from pmix.odkchoices import OdkChoices
 from pmix.odkgroup import OdkGroup
 # from pmix.odkrepeat import OdkRepeat
 # from pmix.odktable import OdkTable
@@ -30,24 +14,8 @@ from pmix.odkgroup import OdkGroup
 test_forms_directory = os_path.dirname(os_path.realpath(__file__))
 
 
-class OdkGroupToDictTest(unittest.TestCase):  # TODO: Add an actual test.
-    """Unit tests for the OdkGroup to_dict method."""
-    def setUp(self):
-        self.data = (
-            ('example_input', 'example_output'),
-            ('example_input', 'example_output')
-        )
-
-    def test_to_dict(self):
-        for i, expected_output in self.data:
-            # output = OdkGroup(i).to_dict
-            output = 'example_output'
-            msg = '\n\nCase:\n{}\nExpected:\n{}'.format(i, expected_output)
-            self.assertTrue(output == expected_output, msg=msg)
-
-
-class OdkGroupRenderHeaderTest(unittest.TestCase):
-    """Unit tests for the OdkGroup render_header method."""
+class OdkGroupTest(unittest.TestCase):
+    """Unit tests for the OdkGroup class."""
     def setUp(self):
         self.data = (
             ({'data': {'type': 'begin group', 'name': 'date_group', 'label::English': '', 'hint::English': '', 'constraint_message::English': '', 'constraint': '', 'required': '', 'appearance': 'field-list', 'default': '', 'relevant': 'today() > date("2017-03-01") and today() < date("2017-11-01")', 'read_only': '', 'calculation': '', 'choice_filter': '', 'image::English': '', 'save_instance': '', 'save_form': '', 'label::Ateso': '', 'label::Luganda': '', 'label::Lugbara': '', 'label::Luo': '', 'label::Lusoga': '', 'label::Ngakarimojong': '', 'label::Runyankole-Rukiga': '', 'label::Runyoro-Rutoro': '', 'hint::Ateso': '', 'hint::Luganda': '', 'hint::Lugbara': '', 'hint::Luo': '', 'hint::Lusoga': '', 'hint::Ngakarimojong': '', 'hint::Runyankole-Rukiga': '', 'hint::Runyoro-Rutoro': '', 'constraint_message::Ateso': '', 'constraint_message::Luganda': '', 'constraint_message::Lugbara': '', 'constraint_message::Luo': '', 'constraint_message::Lusoga': '', 'constraint_message::Ngakarimojong': '', 'constraint_message::Runyankole-Rukiga': '', 'constraint_message::Runyoro-Rutoro': '', 'label': '', 'hint': '', 'constraint_message': '', 'image': '', 'constraint_original': '', 'relevant_original': 'today() > date("2017-03-01") and today() < date("2017-11-01")', 'input_field': None}, 'lang': 'English', 'highlighting': False},
@@ -62,7 +30,7 @@ class OdkGroupRenderHeaderTest(unittest.TestCase):
 
 
 class OdkFormQuestionnaireTest(unittest.TestCase):
-    """Unit tests for the OdkGroup render_header method."""
+    """Unit tests for the OdkForm class."""
     def setUp(self):
         self.data = (
             ({'file': 'FQ.xlsx', 'position': 0},

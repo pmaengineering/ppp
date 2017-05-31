8559c2f6549137ea170a17e28c2880d75767ba54
James Pringle <jameskpringle@gmail.com>
2017-05-31 16:19:53 -0400

    Dict formatting in wbformat

--- pmix/wbformat.py
+++ pmix/wbformat.py
@@ -4,10 +4,10 @@ In the future, it may contain things other than constants.
 """
 
 HL_COLORS = {
-    'HL_YELLOW'     : '#FFFA81',
-    'HL_YELLOW_1'   : '#FDFD96',
-    'HL_ORANGE'     : '#FFD3B6',
-    'HL_RED'        : '#FFAAA5',
-    'HL_GREEN'      : '#85CA5D',
-    'HL_BLUE'       : '#9ACEDF'
+    'HL_YELLOW': '#FFFA81',
+    'HL_YELLOW_1': '#FDFD96',
+    'HL_ORANGE': '#FFD3B6',
+    'HL_RED': '#FFAAA5',
+    'HL_GREEN': '#85CA5D',
+    'HL_BLUE': '#9ACEDF'
 }

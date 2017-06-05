72e7f970489ba1bcc5465afe4bc97646584773dd
joeflack4 <joeflack4@gmail.com>
2017-06-05 12:35:18 -0400

    Refactored OdkGroup.to_html()

--- pmix/ppp/odkgroup.py
+++ pmix/ppp/odkgroup.py
@@ -69,18 +69,6 @@ class OdkGroup:
         i['is_group_header'] = True
         return OdkPrompt(i).to_html(lang, highlighting)
 
-    @staticmethod
-    def render_footer(i, lang, highlighting):
-        """Render footer."""
-        i.row['in_group'] = True
-        return i.to_html(lang, highlighting)
-
-    @staticmethod
-    def render_prompt(i, lang, highlighting):
-        """Render prompt."""
-        i.row['in_group'] = True
-        return i.to_html(lang, highlighting)
-
     def to_html(self, lang, highlighting):
         """Get the html representation of the full group.
 
@@ -93,19 +81,14 @@ class OdkGroup:
         html += TEMPLATE_ENV.get_template('content/group/group-opener.html')\
             .render()
         html += self.render_header(self.opener, lang, highlighting)
-        for i in self.data[0:-1]:
+        for i in self.data:
             if isinstance(i, OdkPrompt):
                 i.row['in_repeat'] = self.in_repeat
-                html += self.render_prompt(i, lang, highlighting)
+                i.row['in_group'] = True
+                html += i.to_html(lang, highlighting)
             elif isinstance(i, OdkTable):
                 i.in_repeat = self.in_repeat
                 html += i.to_html(lang, highlighting)
-        if isinstance(self.data[-1], OdkPrompt):
-            self.data[-1].row['in_repeat'] = self.in_repeat
-            html += self.render_footer(self.data[-1], lang, highlighting)
-        if isinstance(self.data[-1], OdkTable):
-            self.data[-1].in_repeat = self.in_repeat
-            html += self.data[-1].to_html(lang, highlighting)
         # pylint: disable=no-member
         html += TEMPLATE_ENV.get_template('content/group/group-closer.html')\
             .render()

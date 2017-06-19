bcc0f3aff8cf3130ff4d7a58d01e88e019fcd5fb
joeflack4 <joeflack4@gmail.com>
2017-06-19 14:21:03 -0400

    Refactored __init__.py to remove some unnecessary coupling with 'language' parameter.

--- pmix/ppp/__init__.py
+++ pmix/ppp/__init__.py
@@ -6,14 +6,15 @@ from pmix.ppp.error import OdkFormError, OdkChoicesError, \
     InvalidLanguageException
 
 
-def run(inpath, language, output_format, outfile, **kwargs):
+def run(in_file, language=False, output_format=False, out_file=False,
+        **kwargs):
     """Run ODK form conversion.
 
     Args:
-        inpath (str): Path to load source file.
-        language (str): Language to render form.
+        in_file (str): Path to load source file.
         output_format (str): File format to be output.
-        outfile (str): Path to save converted file.
+        out_file (str): Path to save converted file.
+        **language (str): Language to render form.
         **debug (bool): Debugging on or off.
         **highlight (bool): Highlighting on or off.
     Raises:
@@ -21,25 +22,30 @@ def run(inpath, language, output_format, outfile, **kwargs):
         OdkChoicesError: Choice or choice list related.
         OdkFormError: General form related exception.
     """
+    form = OdkForm(file=in_file)
     try:
-        form = OdkForm(file=inpath)
         output = None
         if output_format == 'text':
-            output = form.to_text(language)
+            output = form.to_text(**kwargs)
         elif output_format == 'html' or not output_format:
-            output = form.to_html(language, **kwargs)
-        if outfile:
-            with open(outfile, mode='w', encoding='utf-8') as file:
+            output = form.to_html(**kwargs)
+        if out_file:
+            with open(out_file, mode='w', encoding='utf-8') as file:
                 file.write(output)
         else:
             print(output)
     except InvalidLanguageException as err:
         if len(str(err)):
             raise InvalidLanguageException(err)
+        elif language is False:
+            msg = 'InvalidLanguageException: An unknown error occurred when ' \
+                  'attempting to convert form. If a language was not ' \
+                  'supplied, please supply and try again.'
+            raise InvalidLanguageException(msg)
         elif language not in form.languages:
             msg = 'Specified language not found in form: ' + language
             msg += '\n\nThe form \'{}\' contains the following languages' \
-                   '.\n'.format(inpath)
+                   '.\n'.format(in_file)
             for lang in form.languages:
                 msg += '  * ' + lang + '\n'
             raise InvalidLanguageException(msg[0:-1])

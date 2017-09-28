db8f12772da3e7a902b5df15a916a57572731f01
joeflack4 <joeflack4@gmail.com>
2017-09-28 14:38:10 -0400

    Minor de-linting.

--- pmix/ppp/odkform.py
+++ pmix/ppp/odkform.py
@@ -252,7 +252,6 @@ class OdkForm:
         hlt = kwargs['highlight'] if 'highlight' in kwargs else False
         for index, item in enumerate(data['questionnaire']):
 
-
             if exclusion(item=item, settings=kwargs):
                 continue
 

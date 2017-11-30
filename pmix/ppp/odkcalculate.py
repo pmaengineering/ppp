d1b7753fd29cb5c1f68b5ee121a511e43c99b5de
James <jameskpringle@gmail.com>
2017-11-30 13:04:57 -0500

    Update signature of to_text and to_html

--- pmix/ppp/odkcalculate.py
+++ pmix/ppp/odkcalculate.py
@@ -2,10 +2,10 @@ class OdkCalculate:
     def __init__(self, row):
         self.row = row
 
-    def to_html(self):
+    def to_html(self, *args, **kwargs):
         return ""
 
-    def to_text(self):
+    def to_text(self, *args, **kwargs):
         return ""
 
     def __repr__(self):

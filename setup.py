a2cd66e8ca7dbe9dc731ec4395310eb3717c8215
joeflack4 <joeflack4@gmail.com>
2018-09-14 13:45:13 -0400

    Bugfix. Added missing templates. Should recursively add templates now, so shouldn't happen again.

--- setup.py
+++ setup.py
@@ -1,12 +1,50 @@
 """Setup for Pypi"""
+import os
 from setuptools import setup, find_packages
 
 
 packages = find_packages(exclude=['test'])
 
+
+def get_pkg_data(pkg_name, data_dirs, extensions):
+    """Recusrively get package data.
+
+    This should be called from within setup.py's setuptools.setup() function,
+    e.g. package_data={'my_package': get_pkg_data(pkg_name='my_pckage', ...)}
+
+    Args:
+        pkg_name (str): Name of package containing package data.
+        data_dirs (list):
+        extensions (list):
+
+    Returns:
+        list: List of glob strings where each string represents a terminal
+        branch node (i.e. folder) and a glob of all files ending in designated
+        extension(s).
+
+    Examples:
+        Args: get_pkg_data(pkg_name='my_package',
+        src_dirs=['templates'], extensions=['html'])
+
+        Returns: [
+        'templates/*.html',
+        'templates/content/*.html',
+        'templates/content/prompt/*.html',
+        'templates/content/prompt/inputs/*.html']
+    """
+    pkg_root = \
+        os.path.dirname(os.path.realpath(__file__)) + '/{}/'.format(pkg_name)
+    pkg_data = []
+    for _dir in data_dirs:
+        for ext in extensions:
+            for i, j, y in os.walk(pkg_root + _dir):
+                pkg_data.append(i + '/*.' + ext)
+    return pkg_data
+
+
 setup(
-    name='odk_ppp',  # ppp was already taken
-    version='1.2.1',
+    name='odk_ppp',  # The name 'ppp' was already taken.
+    version='1.2.2',
     author='Joe Flack',
     author_email='jflack@jhu.edu',
     url='http://www.pma2020.org',
@@ -15,15 +53,9 @@ setup(
         'ppp': 'ppp'
     },
     package_data={
-        'ppp': [
-            'templates/*.html',
-            'templates/content/*.html',
-            'templates/content/group/*.html',
-            'templates/content/prompt/*.html',
-            'templates/content/prompt/inputs/*.html',
-            'templates/content/repeat/*.html',
-            'templates/content/table/*.html'
-        ]
+        'ppp': get_pkg_data(pkg_name='ppp',
+                            data_dirs=['templates'],
+                            extensions=['html'])
     },
     license='LICENSE.txt',
     description='Converts XlsForm Excel files (ODK, SurveyCTO, etc) into '
